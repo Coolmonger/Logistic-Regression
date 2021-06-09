d2e8d2d1394dd0fe9fe7bfc741c597d1088ffec7
Coolmonger <40465458+Coolmonger@users.noreply.github.com>
2021-06-09 03:57:27 +0300

    Update Logistic Regression.py

--- Logistic Regression.py
+++ Logistic Regression.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 import numpy as np
@@ -11,24 +8,14 @@ import matplotlib.pyplot as plt
 import torch
 from tqdm.notebook import tqdm
 
-
-# In[5]:
-
-
 from torchvision import datasets, transforms
 
 mnist_train = datasets.MNIST(root="./datasets", train=True, transform=transforms.ToTensor(), download=True)
 mnist_test = datasets.MNIST(root="./datasets", train=False, transform=transforms.ToTensor(), download=True)
 
 
-# In[6]:
-
-
 print("Number of MNIST training examples: {}".format(len(mnist_train)))
 print("Number of MNIST test examples: {}".format(len(mnist_test)))
-
-
-# In[8]:
 
 
 image, label = mnist_train[3]
@@ -38,9 +25,6 @@ print("Reshaped image shape: {}".format(image.shape))
 plt.imshow(image, cmap="gray")
 
 print("The label for this image: {}".format(label))
-
-
-# In[3]:
 
 
 train_loader = torch.utils.data.DataLoader(mnist_train, batch_size=100, shuffle=True)
